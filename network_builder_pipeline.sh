ba27309f43bf9a260adf3ec4fd51b2df0cfae356
Bree Cummins <breecummins@gmail.com>
2016-04-18 09:46:41 -0600

    testing if I can get partial matches

--- network_builder_pipeline.sh
+++ network_builder_pipeline.sh
@@ -14,20 +14,18 @@ NUMEDGES=2 # add edges of rank 1 to n singly and in pairs
 TIMESERIES="datafiles/wrair2015_v2_fpkm-p1_s19.tsv"
 TS_TYPE="row"  # or 'col', type of time series file format
 TS_TRUNCATION=42 #cut after 42 time units (NOT after index 42)
-SCALING_FACTOR=0.05   # between 0 and 1; 0 = most restrictive partial order; 1 = least restrictive
+SCALING_FACTOR=0.01   # between 0 and 1; 0 = most restrictive partial order; 1 = least restrictive
 
 python ./makeFilesForAnalysis.py $STARTINGFILE $LEMFILE $RANKEDGENES $NUMNODES $NUMEDGES $TIMESERIES $TS_TYPE $TS_TRUNCATION $SCALING_FACTOR
 
-exit 0
+# DSGRN=/share/data/bcummins/DSGRN
+# SIGNATURES=$DSGRN/software/Signatures/bin/Signatures
+# PATTERNMATCH=$DSGRN/software/PatternMatch/bin/PatternMatchDatabase
 
-DSGRN=/share/data/bcummins/DSGRN
-SIGNATURES=$DSGRN/software/Signatures/bin/Signatures
-PATTERNMATCH=$DSGRN/software/PatternMatch/bin/PatternMatchDatabase
-
-for i in $( ls inputfiles/networks/*); do
-	NUM=$(echo $i | sed -e s/[^0-9]//g);
-	DATABASENAME="./databases/database$NUM.db";
-	qsub script_for_qsub.sh $SIGNATURES $i $DATABASENAME $PATTERNMATCH "./inputfiles/POs/partialorder$NUM.json"
-done
+# for i in $( ls inputfiles/networks/*); do
+# 	NUM=$(echo $i | sed -e s/[^0-9]//g);
+# 	DATABASENAME="./databases/database$NUM.db";
+# 	qsub script_for_qsub.sh $SIGNATURES $i $DATABASENAME $PATTERNMATCH "./inputfiles/POs/partialorder$NUM.json"
+# done
 
 
