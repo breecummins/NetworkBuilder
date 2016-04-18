986c41c4b64191668e71c48356c7b95e5c543d3b
Bree Cummins <breecummins@gmail.com>
2016-04-18 16:20:20 -0600

    be sure to clear results file

--- concatenateresults.sh
+++ concatenateresults.sh
@@ -3,13 +3,13 @@
 SAVEFILE=$1 #json
 OUTPUTDIR=$2 #where all the results files are
 
-printf "[" >> $SAVEFILE
+printf "[" > $SAVEFILE #write to file
 
 for i in $(ls $OUTPUTDIR/*); do
-	cat $i >> $SAVEFILE;
+	cat $i >> $SAVEFILE; #append to file
 	printf ',' >> $SAVEFILE;
 done
 
-printf '%s\n' '$' 's/.$/]/' wq | ex $SAVEFILE
+printf '%s\n' '$' 's/.$/]/' wq | ex $SAVEFILE #replace last comma with closing bracket
 
 # now delete stuff in OUTPUTDIR
