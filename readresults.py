a3c7a4c88df16b74326b707547bf8fa5740e7b5a
Bree Cummins <breecummins@gmail.com>
2016-04-19 13:34:33 -0600

    added function to read multiple results files

--- readresults.py
+++ readresults.py
@@ -1,4 +1,4 @@
-import json,sys
+import json,sys,glob
 
 def makestats(fname='results.json'):
     with open(fname,'r') as f:
@@ -9,5 +9,14 @@ def makestats(fname='results.json'):
         fcpm = n["StableFCMatchesParameterCount"]
         print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/{0:.2f}'.format((float(fcpm)/fcpc) * 100)
 
+def makestats_multiplefile(DIR='outfiles'):
+    for fname in glob.glob(DIR+'/results*.txt'):
+        with open(fname,'r') as f:
+            n = json.load(f)
+        pc = n["ParameterCount"]
+        fcpc = n["StableFCParameterCount"]
+        fcpm = n["StableFCMatchesParameterCount"]
+        print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/{0:.2f}'.format((float(fcpm)/fcpc) * 100)
+
 if __name__=='__main__':
-    makestats(sys.argv[1]) 
+    makestats_multiplefile(sys.argv[1]) 
