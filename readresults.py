38a8614901a99bde42814b017dc66505c0667a9e
Bree Cummins <breecummins@gmail.com>
2016-04-19 13:44:09 -0600

    cosmetic change

--- readresults.py
+++ readresults.py
@@ -10,7 +10,7 @@ def makestats(fname='results.json'):
         if fcpc > 0:
             print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/{0:.2f}'.format((float(fcpm)/fcpc) * 100)
         else:
-            print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/{NaN}'
+            print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/NaN'
 
 
 def makestats_multiplefile(DIR='outfiles/'):
@@ -23,7 +23,7 @@ def makestats_multiplefile(DIR='outfiles/'):
         if fcpc > 0:
             print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/{0:.2f}'.format((float(fcpm)/fcpc) * 100)
         else:
-            print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/{NaN}'
+            print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/NaN'
 
 if __name__=='__main__':
     makestats_multiplefile(sys.argv[1]) 
