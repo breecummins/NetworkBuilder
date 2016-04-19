ce9e38fd1fbc7764773fe19c168b5b9ba487423f
Bree Cummins <breecummins@gmail.com>
2016-04-19 13:39:58 -0600

    fixed file read bug

--- readresults.py
+++ readresults.py
@@ -9,8 +9,8 @@ def makestats(fname='results.json'):
         fcpm = n["StableFCMatchesParameterCount"]
         print str(pc) + '/' + str(fcpc) + '/' + str(fcpm) + '/{0:.2f}'.format((float(fcpm)/fcpc) * 100)
 
-def makestats_multiplefile(DIR='outfiles'):
-    for fname in glob.glob(DIR+'/results*.txt'):
+def makestats_multiplefile(DIR='outfiles/'):
+    for fname in glob.glob(DIR+'results*.txt'):
         with open(fname,'r') as f:
             n = json.load(f)
         pc = n["ParameterCount"]
