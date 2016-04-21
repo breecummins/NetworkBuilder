fbd8508955ddd7aedea35ffa6f3d9682c3d71540
Bree Cummins <breecummins@gmail.com>
2016-04-21 14:18:35 -0600

    fixed bug in which starting graph was getting reassigned at every iteration of a for loop. I was passing a list of lists without explicitly copying the internal lists, and the internal lists were distorting.

--- random_networkbuilder.py
+++ random_networkbuilder.py
@@ -1,5 +1,8 @@
 import fileparsers
-import random
+import random,sys
+
+def getRandomInt(n):
+    return random.randrange(n)
 
 def addEdge(graph,reg):
     n = len(graph)
@@ -21,7 +24,7 @@ def addEdge(graph,reg):
     elif endnode in graph[startnode]:
         # swap regulation if edge already exists
         ind = graph[startnode].index(endnode)
-        reg[startnode][ind] = thisreg
+        thisreg = reg[startnode][ind]
         reg[startnode][ind] = 'a'*(thisreg == 'r') + 'r'*(thisreg == 'a')
     else:
         # otherwise add edge with random sign
@@ -33,20 +36,17 @@ def addEdge(graph,reg):
 def addNode(graph,reg):
     n = len(graph)
     # the new node will have index n
-    # pick an incoming edge for n
-    innode = getRandomInt(n)
+    # pick an incoming edge for n from the existing network (i.e. don't pick node n)
+    innode = getRandomInt(n) # NOT getRandomInt(n+1)
     inreg = getRandomInt(2)
     graph[innode].append(n)
-    reg[innode].append('a'*regbool + 'r'*(not regbool))
-    # pick an outgoing edge for n 
+    reg[innode].append('a'*inreg + 'r'*(not inreg))
+    # pick an outgoing edge for n from the existing network
     outnode = getRandomInt(n)
     outreg = getRandomInt(2)
     graph.append([outnode])
-    reg.append(['a'*regbool + 'r'*(not regbool)])
+    reg.append(['a'*outreg + 'r'*(not outreg)])
     return graph, reg
-
-def getRandomInt(n):
-    return random.randrange(n)
 
 def perturbNetwork(graph,reg):
     keepgoing = 1
@@ -60,14 +60,25 @@ def perturbNetwork(graph,reg):
     return (graph,reg)
 
 def makeNearbyNetworks(starting_network_filename,N,savename = 'network_'):
+    random.seed()
     _,starting_graph,starting_regulation,_ = fileparsers.getGraphFromNetworkFile(network_filename=starting_network_filename)
-    networks = [None] * N
-    for i in range(N):
-        networks[i] = perturbNetwork(starting_graph,starting_regulation)
-    uniq_ntw = list(set(networks))
-    for i,(graph,reg) in enumerate(uniq_ntw):
+    networks=[]
+    while len(networks) < int(N):
+        # below: the lists within the starting graph must be explicitly copied or else the starting graph gets reassigned within perturbNetwork
+        sg = [list(graph) for graph in starting_graph]  
+        sr = [list(reg) for reg in starting_regulation]
+        net = perturbNetwork(sg,sr)
+        if net not in networks:
+            networks.append(net)
+    for i,(graph,reg) in enumerate(networks):
         node_list = ['x'+str(k) for k in range(len(graph))]
         essential = [True] * len(graph)
         fname = savename+str(i)+'.txt'
-        fileparsers.createNetworkFile(node_list,graph,reg,ess,fname,save2file=True)
+        fileparsers.createNetworkFile(node_list,graph,reg,essential,fname,save2file=True)
+
+if __name__ == '__main__':
+    # fname="/Users/bcummins/GIT/DSGRN/networks/5D_2016_02_08_cancer_withRP_essential.txt"
+    # N = 200
+    # savename = 'networks/randnet_'
+    makeNearbyNetworks(*sys.argv[1:])
 
